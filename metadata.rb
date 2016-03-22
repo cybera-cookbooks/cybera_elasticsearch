20a66d99f58fcf42b28b303820f6b9a4c37853bd
Andrew Klaus <andrewklaus@gmail.com>
2016-03-22 21:00:25 +0000

    Updating with what's actually on chef server

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures cybera_elasticsearch'
 long_description 'Installs/Configures cybera_elasticsearch'
-version          '0.1.0'
+version          '0.3.0'   # 03/2016 sprint
 
 depends 'apt'
 depends "java"
